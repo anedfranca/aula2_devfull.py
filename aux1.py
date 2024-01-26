06d6f4af6fd6936643cfc83095da345904437806
Ane França <127628489+anedfranca@users.noreply.github.com>
2024-01-26 18:12:29 -0300

    Add files via upload

---
+++ aux1.py
@@ -0,0 +1,15 @@
+nm = input ('nome: ')
+
+for letra in nm:
+    print(letra)
+
+vezes = int(input("Vezes? "))
+
+for i in range(1,vezes+1):
+    print('='*i)
+
+nome = 'Ane'
+print('oi',754,nome)
+print(f'este é o print do {nome}')
+print('oi, '+nome+', tudo bem?')
+print('='*10)
